1f9d84d75cad2ba0d5bc122787a51f2635a4fb32
noegratico <noe.gratico@gmail.com>
2023-03-26 12:49:17 +0800

    route

--- aqua-project/src/App.js
+++ aqua-project/src/App.js
@@ -3,21 +3,22 @@ import SideView from './components/SideView';
 import DashboardView from './components/DashboardView';
 import GreenHousePages from './pages/GreenHousePages';
 import './App.css';
-
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter as  Router, Routes, Route, } from "react-router-dom";
 
 function App() {
 
   return(
    <>
    <Header/>
-
    <Router>
-      <Route path='/'>
-        <Route path="greenhousepages" component={<GreenHousePages/>}/>
-      </Route>
-    </Router>
+    <Routes>
+   <Route path="/greenhousepage" element={ <GreenHousePages/>}/>
+   </Routes>
+   </Router>
+  
+   
+   
+ 
  
 
    </>)
