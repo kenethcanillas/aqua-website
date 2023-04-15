86442a9a663c702bb624a88d0bba39a7bafc8745
KENNN <84948825+kenethcanillas@users.noreply.github.com>
2023-04-15 12:36:08 +0800

    dashboard updates

--- aqua-project/src/components/SideView.js
+++ aqua-project/src/components/SideView.js
@@ -1,119 +1,109 @@
 import '../App.css';
 import { Icon } from '@iconify/react';
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+
+
+function getWeatherCategory(code) {
+  if (code === 0) {
+    return "Clear Sky";
+  } else if (code >= 1 && code <= 44) {
+    return "Party Cloud";
+  } else if (code >= 45 && code <= 47) {
+    return "Fog";
+  } else if (code >= 48 && code <= 55) {
+    return "Drizzle";
+  } else if (code >= 56 && code <= 77) {
+    return "Rain";
+  } else if (code >= 80 && code <= 94) {
+    return "Rain Shower";
+  } else if (code >= 95 && code <= 99) {
+    return "Thunderstorm";
+  } else {
+    return " ";
+  }
+};
 
 
 function SideView() {
-  
-  
+
+
+
+
+
   const [date, setDate] = useState(new Date());
 
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setDate(new Date());
-      }, 1000);
-      return () => clearInterval(intervalId);
-    }, []);
- 
-    const [temperature, setTemperature] = useState(null);
-    const [condition, setCondition] = useState(null);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  const [geoMeteo, setgeoMeteoState] = useState({});
+  const [openMeteo, setOpenMeteoState] = useState({});
+
 
   useEffect(() => {
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=14.70&lon=121.03&appid=5785d4993a0c6b5ba049b85ac0d20a5f`)
+    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=Quezon+City&count=10&language=en&format=json`)
 
       .then(response => response.json())
-      .then(data => {
-        const temp = Math.round(data.main.temp - 273.15);
-        const condition = data.weather[0].description;
-        setTemperature(temp);
-        setCondition(condition);
+      .then(geodata => {
+          // console.log(geodata);
+
+          setgeoMeteoState({lat: geodata.results[0].latitude , long: geodata.results[0].longitude, cityname: geodata.results[0].name, country: geodata.results[0].country });
+
       })
       .catch(error => console.error(error));
   }, []);
 
-  /// OPEN-METEO  * fetch data error
+  // const latitude = geoMeteo.lat;
+  // const longitude = geoMeteo.long;
 
- 
+  const [cordinates, setCordinates] = useState({});
 
-// const [temperature, setTemperature] = useState(null);
-// const [weatherCondition, setWeatherCondition] = useState(null);
-// const [isLoading, setIsLoading] = useState(true);
-// const [errorMessage, setErrorMessage] = useState(null);
 
-//   useEffect(() => {
-//     axios.get('https://api.open-meteo.com/v1/forecast?latitude=14.66&longitude=121.02&hourly=temperature_2m,weathercode&daily=weathercode&current_weather=true&past_days=1&forecast_days=1&timezone=Asia%2FSingapore')
-//     .then(response => {
-//       if (response.data && response.data.data && response.data.data.length > 0) {
-//         setTemperature(response.data.data[0].temperature_2m);
-//         setWeatherCondition(mapWeatherCondition(response.data.data[0].weathercode));
-//       } else {
-//         setErrorMessage('No data found.');
-//       }
-//     })
-//     .catch(error => {
-//       console.log(error);
-//       setErrorMessage('Error fetching data.');
-//     })
-//     .finally(() => {
-//       setIsLoading(false);
-//     });
-// }, []);
-// const mapWeatherCondition = (code) => {
-//   switch (code) {
-//     case 0:
-//       return 'Clear sky';
-//     case 1:
-//       return 'Mostly clear';
-//     case 2:
-//       return 'Partly cloudy';
-//     case 3:
-//       return 'Overcast';
-//     case 4:
-//       return 'Fog';
-//     case 5:
-//       return 'Rain showers';
-//     case 6:
-//       return 'Rain';
-//     case 7:
-//       return 'Thunderstorm';
-//     case 8:
-//       return 'Light snow';
-//     case 9:
-//       return 'Snow';
-//     case 10:
-//       return 'Heavy snow';
-//     default:
-//       return 'Unknown';
-//   }
-// };
 
-// if (isLoading) {
-//   return <p>Loading...</p>;
-// }
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCordinates({ latitude: position.coords.latitude, longitude: position.coords.longitude });
+      setCordinates(position.coords)
+    })
 
-// if (errorMessage) {
-//   return <p>{errorMessage}</p>;
-// }
- return (
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${cordinates.latitude}&longitude=${cordinates.longitude}&hourly=temperature_2m,weathercode&current_weather=true`)
+
+      .then(response => response.json())
+      .then((data) => {
+        // console.log(data);
+        setOpenMeteoState({ temp: data.current_weather.temperature, code: data.current_weather.weathercode });
+
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  const weatherCategory = getWeatherCategory(openMeteo.code);
+
+  return (
     <>
-    
-      <div className='sideview-container mt-4'> 
+      {/* {geoMeteo.lat}
+    {geoMeteo.long} */}
+      <div className='sideview-container mt-4'>
         <div className='weather-row'>
           <div className="temp-column">
-            <h1 className='temperature'>{temperature}{<Icon icon="tabler:temperature-celsius" color="#3f3f3f"/>} </h1>
+            <h1 className='temperature'>{openMeteo.temp}{<Icon icon="tabler:temperature-celsius" color="#3f3f3f" />} </h1>
             <p>Temperature</p>
-              <div className='time'>
-                <p className="hours "> <span>{<Icon icon="ic:round-access-time" color="#3f3f3f" />}</span>{date.toLocaleTimeString([], {hour: '2-digit' , minute:'2-digit'})}</p>
-                <p className="dates"><span>{<Icon icon="material-symbols:calendar-month-outline" />}</span>{date.toLocaleDateString([], {month:'long', day:'2-digit', year:'numeric'})}</p>
-              </div>
+            <div className='time'>
+              <p className="hours "> <span>{<Icon icon="ic:round-access-time" color="#3f3f3f" />}</span>{date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+              <p className="dates"><span>{<Icon icon="material-symbols:calendar-month-outline" />}</span>{date.toLocaleDateString([], { month: 'long', day: '2-digit', year: 'numeric' })}</p>
+            </div>
           </div>
           <div className='weather-condition-column'>
-          <div className='condition'>{<Icon icon="ic:outline-cloud-queue" width="42px" height="42px"/>}<span className='output'>{condition}</span></div>
-            <p className='place'>Quezon City, Philippines</p>
-            {/* {temperature && <p>The temperature is {temperature} degrees Celsius.</p>}
-            {weatherCondition && <p>The weather condition is {weatherCondition}.</p>} */}
+            <div className='condition'>
+              {<Icon icon="ic:outline-cloud-queue" width="42px" height="42px" />}
+              <span className='output'>{weatherCategory}</span>
+            </div>
+            <p className='place'>{geoMeteo.cityname}, <span>{geoMeteo.country}</span></p>
           </div>
         </div>
       </div>
@@ -124,4 +114,3 @@ function SideView() {
 
 export default SideView;
 
-  
